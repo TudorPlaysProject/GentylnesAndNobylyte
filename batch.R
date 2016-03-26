218a19b12181d2e5acc51525d42e61bbb68d6d83
Paul Evans <decretist@gmail.com>
2016-03-25 18:30:10 -0700

    Comment nouns and verbs out of wordlist.txt

---
+++ batch.R
@@ -0,0 +1,9 @@
+#
+# Paul Evans (pevans@sandiego.edu)
+# 23 March 2016
+#
+setwd("~/Work/GentylnesAndNobylyte")
+library(stylo)
+results = stylo()
+# summary(results)
+print(results$features.actually.used)
