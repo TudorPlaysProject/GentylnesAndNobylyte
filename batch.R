ea7832543a8dd9115828df48bd64e83faf1804ac
Paul Evans <decretist@gmail.com>
2016-03-30 11:26:52 -0700

    Update R batch script and images for RSA
    In advance of Maura's presentation at Renaissance Society of America,
    check in revised batch.R to allow others to reproduce our results.
    Also check in comparison JPEG files, and changes to .gitignore to
    allow the JPEGs to be committed.

--- batch.R
+++ batch.R
@@ -4,6 +4,25 @@
 #
 setwd("~/Work/GentylnesAndNobylyte")
 library(stylo)
-results = stylo()
-# summary(results)
-print(results$features.actually.used)
+stylo.results = stylo(
+  gui = FALSE,
+  features = "wordlist0.txt",
+  mfw.list.cutoff = 250,
+  write.jpg.file = TRUE,
+  custom.graph.filename = "G_and_N_CA_100_MFWs"
+)
+# summary(stylo.results)
+print(stylo.results$features.actually.used)
+#
+##
+#
+stylo.results = stylo(
+  gui = FALSE,
+  features = "wordlist1.txt",
+  mfw.min = 65, mfw.max = 65,
+  mfw.list.cutoff = 250,
+  write.jpg.file = TRUE,
+  custom.graph.filename = "G_and_N_CA_65_MFWs"
+)
+# summary(stylo.results)
+print(stylo.results$features.actually.used)
