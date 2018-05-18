a0f9609be376de2a42c813bdf3b38de23627ce27
Paul Evans <decretist@gmail.com>
2018-05-17 18:15:08 -0700

    Modify R script
    Now produces JPEG files for Cluster Analysis based on inital wordlist
    and Cait's edited wordlist.

--- Working/TudorPlays.R
+++ Working/TudorPlays.R
@@ -2,13 +2,28 @@
 # Paul Evans (pevans@sandiego.edu)
 # 27 April 2018
 #
-setwd("~/Documents/GitHub/GentylnesAndNobylyte/Working")
 library(stylo)
 stylo.results = stylo(
   gui = FALSE,
-  features = "wordlist_cait.txt",
-  mfw.list.cutoff = 300
+  features = "wordlist_init.txt",
+  mfw.min = 65, mfw.max = 65,
+  mfw.list.cutoff = 300,
+  write.jpg.file = TRUE,
+  custom.graph.title = "Initial List",
+  custom.graph.filename = "Initial_CA_65_MFWs_Culled_0"
 )
-summary(stylo.results)
+# summary(stylo.results)
+print(stylo.results$features.actually.used)
+#
+stylo.results = stylo(
+  gui = FALSE,
+  features = "wordlist_cait.txt",
+  mfw.min = 65, mfw.max = 65,
+  mfw.list.cutoff = 300,
+  write.jpg.file = TRUE,
+  custom.graph.title = "Cait's List",
+  custom.graph.filename = "Cait_CA_65_MFWs_Culled_0"
+)
+# summary(stylo.results)
 print(stylo.results$features.actually.used)
 
