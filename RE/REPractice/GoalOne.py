b4c60e7532f24cdd10d1dbbb3c75118b7bbd6ce4
Jordan <jmatuszewski@sandiego.edu>
2018-11-07 18:41:02 -0800

    Debugged, replacement works.

--- RE/REPractice/GoalOne.py
+++ RE/REPractice/GoalOne.py
@@ -4,22 +4,23 @@ import re
 
 def main():
     OrigText = open("WordListBefore.txt", "r")
-NewText = open("WordListAfter.txt", "w")
-line = OrigText.readline()
+    NewText = open("WordListAfter.txt", "w")
+    line = OrigText.readline()
+    RegexOne = re.compile(r'(([a-z])*y([a-z])*)') #MatchesAllYWords
+    RegexTwo= re.compile(r'([aeiou])+u/*([aeiou])') #MatchesAnyWordWithUInsteadOfV
 
-RegexOne = re.compile(r'(([A-Z])*y)') #MatchesAllYWords
-RegexTwo= re.compile(r'([aeiou])+u/*([aeiou])') #MatchesAnyWordWithUInsteadOfV
-
-while line: 
-    match = RegexOne.search(line)
-    matchTwo = RegexTwo.search(line)
-    if match:
-         printNewText(match)
-    if matchTwo:
-         printNewText(matchTwo) 
-
-OrigText.close()
-NewTest.close()
+    while line: 
+        match = RegexOne.search(line)
+        matchTwo = RegexTwo.search(line)
+        if match:
+            print format(match.group(0))
+            print re.sub('y','i',line)
+        if matchTwo:
+            print format(matchTwo.group(0)) 
+            print re.sub('u', 'v', line)        
+	line = OrigText.readline()
+    OrigText.close()
+    NewText.close()
 
 if __name__ == '__main__':
-  main()
+    main()
