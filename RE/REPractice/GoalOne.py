d299062df910276a96a5a7de8e23996c8e213369
Paul Evans <decretist@gmail.com>
2018-11-08 13:58:48 -0800

    Change while loop to for loop

--- RE/REPractice/GoalOne.py
+++ RE/REPractice/GoalOne.py
@@ -5,20 +5,19 @@ import re
 def main():
     OrigText = open("WordListBefore.txt", "r")
     NewText = open("WordListAfter.txt", "w")
-    line = OrigText.readline()
+    lines = OrigText.readlines()
     RegexOne = re.compile(r'(([a-z])*y([a-z])*)') #MatchesAllYWords
     RegexTwo= re.compile(r'([aeiou])+u/*([aeiou])') #MatchesAnyWordWithUInsteadOfV
 
-    while line: 
+    for line in lines:
         match = RegexOne.search(line)
         matchTwo = RegexTwo.search(line)
         if match:
-            NewText.write(format(match.group(0)))
+            # NewText.write(format(match.group(0)))
             NewText.write(re.sub('y','i',line))
         if matchTwo:
-            NewText.write(format(matchTwo.group(0))) 
+            # NewText.write(format(matchTwo.group(0))) 
             NewText.write(re.sub('u', 'v', line))        
-        line = OrigText.readline()
     OrigText.close()
     NewText.close()
 
