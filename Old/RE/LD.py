8e4ad7233a1a608f38f684682aa98af00b493859
Jordan <jmatuszewski@sandiego.edu>
2019-02-04 23:00:10 -0800

    Program that generates words with Levenstein distance of one into new word list. Along with texts before and after.

--- Old/RE/LD.py
+++ Old/RE/LD.py
@@ -1,22 +1,19 @@
 #!/usr/local/bin/python3
 # Jordan Matuszewski
-# 09 Janruary 2019
-# Example
-# Paul Evans
-# 30 January 2019
+# 01 February 2019
 
 import re
 import Levenshtein
 def main():
-    with open('WLAfterHamming.txt', 'w') as the_file:
-        with open('WLBeforeHamming.txt', 'r') as f_in:
+    with open('WLAfterLD.txt', 'w') as the_file:
+        with open('WLBeforeLD.txt', 'r') as f_in:
             file = f_in.read()
             words_one = re.split('\W', file)
             words_two = words_one
             for word_one in words_one:
                 for word_two in words_two:
                     if len(word_one) == len(word_two):
-                        if Levenshtein.hamming(word_one, word_two) == 1:
+                        if Levenshtein.distance(word_one, word_two) == 1:
                             the_file.write(word_one +" "+ word_two + '\n')
 
 if __name__ == '__main__':
