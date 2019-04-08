200fa7f1294de95e2421e4bbd4f179d002c41da0
Paul Evans <decretist@gmail.com>
2019-04-08 08:22:27 -0700

    Set up temporary work day folder

---
+++ WorkDay/README.md
@@ -0,0 +1,23 @@
+I've found four copies of the file X_Gentylnes_and_Nobylyte.txt in
+the GitHub repository, representing three distinct versions.
+
+The oldest version is represented by the files:
+./GentylnesAndNobylyte/Old/RE/X_Gentylnes_and_Nobylyte.txt
+./GentylnesAndNobylyte/RSA/corpus/X_Gentylnes_and_Nobylyte.txt
+This is the version that Maura used as the basis for her Renaissance
+Society of America presentation in March 2016. It has a word count
+of 1591 (Unix command wc -w) and a checksum of 19233 8 (Unix command
+sum).
+
+The intermediate version is represented by the file:
+./GentylnesAndNobylyte/Working/corpus/X_Gentylnes_and_Nobylyte.txt
+This version has a complicated history: I asked Maura to generate
+3000-word samples, and then I scrubbed what she gave me in various
+ways to make it suitable for analysis using stylo. It has a word
+count of 3871 and a checksum of 54556 20. Use this version as the
+input for your conversion program.  The most recent version is
+represented by the file:
+./GentylnesAndNobylyte/Working/X_Gentylnes_and_Nobylyte.txt
+This is the May 2018 Victoria Maretti version. It has a word count
+of 3731 and a checksum of 30048 19. Compare this version with the
+output of your conversion program.
---
+++ WorkDay/VictoriaAutoEdits.py
@@ -0,0 +1,47 @@
+#!/usr/local/bin/python3
+# Jordan Matuszewski
+# 12 February 2019
+
+import re
+
+def main():
+
+    with open('X_Gentylnes_and_Nobylyte_AfterER.txt', 'w') as Output:
+        with open('GentylnesAndNobylyte.txt', 'r') as Input:
+
+
+            lines = Input.readlines()
+
+            for line in lines:
+
+                line = re.sub('&',"and", line, flags = re.IGNORECASE)
+                line = re.sub('〈◊+〉',"", line, flags = re.IGNORECASE)
+                line = re.sub('\b[^aeiou\s]{1}\b',"", line, flags = re.IGNORECASE)
+                line = re.sub('(page\s+\[unnumbered\])',"", line, flags = re.IGNORECASE)
+                line = re.sub('¶',"", line)
+                line = re.sub('eue',"eve", line, flags = re.IGNORECASE) #haue to have
+                line = re.sub('lyst',"lest", line, flags = re.IGNORECASE) #noblyst to noblest
+                line = re.sub('aue',"ave", line, flags = re.IGNORECASE) #euer to ever
+                line = re.sub('hyn(?=[^aeiou])',"hin", line, flags = re.IGNORECASE) #thyng to thing
+                line = re.sub('\b(y)(?=[^ts])[a-z]{1}\b', "wi", line, flags = re.IGNORECASE) #wyse to wise
+                line = re.sub('\bry(?=[^aeiou])+', "ri", line, flags = re.IGNORECASE) #ryches to riches
+                line = re.sub('eri\b',"ery", line, flags = re.IGNORECASE) #eri to ery (doesn't work, suspect it has something to do with eue being changed)
+                line = re.sub('\bbi\b',"by", line, flags = re.IGNORECASE) #doesnt work with word boundary, but changes false positives without
+                line = re.sub('∣',"", line)
+                line = re.sub('\b[^aeiou\s]{1}\b',"", line)
+                # line = re.sub('[p̄]',"pir", line, flags = re.IGNORECASE)
+                line = re.sub('[ā]',"am", line, flags = re.IGNORECASE)
+                line = re.sub('((?<! c)ō)',"om", line, flags = re.IGNORECASE)
+                line = re.sub('((?<=c)ō)',"on", line, flags = re.IGNORECASE)
+                line = re.sub('[ē]',"em", line, flags = re.IGNORECASE)
+                line = re.sub('[ī]',"im", line, flags = re.IGNORECASE)
+                line = re.sub('[ū]',"um", line, flags = re.IGNORECASE)
+                line = re.sub(r'{(\w+)}', '\\1', line)
+                line = re.sub('((?<=[aeiou])u(?=[aeiou]))',"v", line, flags = re.IGNORECASE)
+                line = re.sub('((?<=[aeiou])i(?=[aeiou]))',"j", line, flags = re.IGNORECASE)
+                Output.write(line)
+
+
+if __name__ == '__main__':
+    main()
+
